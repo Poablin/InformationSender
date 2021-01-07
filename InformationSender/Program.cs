b916ad020b7e37a26be2630faba0c7eb40b2137b
Poablin <65161335+Poablin@users.noreply.github.com>
2021-01-07 13:11:41 +0100

    Update Program.cs

--- InformationSender/Program.cs
+++ InformationSender/Program.cs
@@ -15,8 +15,11 @@ namespace InformationSender
             var testList = new DocumentModel[1000];
             for (int i = 0; i < 1000; i++)
             {
+                var randomNum = new Random();
                 var testFile = new DocumentModel();
-                testFile.Filename = "2" + new Random().Next(9000000, 9999999).ToString() + ".pdf";
+                testFile.Filename = "2" + randomNum.Next(0000000, 9999999).ToString() + ".pdf";
+                testFile.InvoiceNumber = "1" + randomNum.Next(000000, 99999).ToString();
+                testFile.CustomerNumber = "000001";
                 testList[i] = testFile;
             }
 
