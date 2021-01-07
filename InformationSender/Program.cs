9b244d84c8b8ae830c75ad960194bdfd20664463
Poablin <65161335+Poablin@users.noreply.github.com>
2021-01-07 12:07:26 +0100

    Update Program.cs

--- InformationSender/Program.cs
+++ InformationSender/Program.cs
@@ -13,23 +13,23 @@ namespace InformationSender
             var model = new DocumentModel();
 
             var xmlTree1 = new XElement("File",
-            new XElement("Owner", 1),
-            new XElement("DocType", 2)
+            new XElement("Owner", model.Owner),
+            new XElement("DocType", model.DocType)
             );
 
             var xmlTree2 =
             new XElement("FileInfo",
-                new XElement("Filename", 3),
-                new XElement("InvoiceNumber", 4),
-                new XElement("CustomerNumber", 5),
-                new XElement("Name", 6),
-                new XElement("Addr1", 7),
-                new XElement("ZipCode", 8),
-                new XElement("ZipName", 9),
-                new XElement("CountryCode", 10),
-                new XElement("IssueDate", 11),
-                new XElement("DueDate", 12),
-                new XElement("TotalAmount", 13)
+                new XElement("Filename", model.Filename),
+                new XElement("InvoiceNumber", model.InvoiceNumber),
+                new XElement("CustomerNumber", model.CustomerNumber),
+                new XElement("Name", model.Name),
+                new XElement("Addr1", model.Addr1),
+                new XElement("ZipCode", model.ZipCode),
+                new XElement("ZipName", model.ZipName),
+                new XElement("CountryCode", model.CountryCode),
+                new XElement("IssueDate", model.IssueDate),
+                new XElement("DueDate", model.DueDate),
+                new XElement("TotalAmount", model.TotalAmount)
             );
 
            var doc = new XDocument(xmlTree1);
@@ -45,3 +45,28 @@ namespace InformationSender
         }
     }
 }
+
+
+
+
+
+
+//var xmlTree1 = new XElement("File",
+//new XElement("Owner", 1),
+//new XElement("DocType", 2)
+//);
+
+//var xmlTree2 =
+//new XElement("FileInfo",
+//    new XElement("Filename", 3),
+//    new XElement("InvoiceNumber", 4),
+//    new XElement("CustomerNumber", 5),
+//    new XElement("Name", 6),
+//    new XElement("Addr1", 7),
+//    new XElement("ZipCode", 8),
+//    new XElement("ZipName", 9),
+//    new XElement("CountryCode", 10),
+//    new XElement("IssueDate", 11),
+//    new XElement("DueDate", 12),
+//    new XElement("TotalAmount", 13)
+//);
