c8d07f8d89d64a065d6f6f5a1b445530228c56ef
Poablin <65161335+Poablin@users.noreply.github.com>
2021-01-07 13:02:54 +0100

    Update Program.cs

--- InformationSender/Program.cs
+++ InformationSender/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace InformationSender
+﻿using System;
+using System.Collections.Generic;
+
+namespace InformationSender
 {
     class Program
     {
@@ -7,6 +10,15 @@
             var model = new DocumentModel();
             var fileList = new DocumentModel[1];
             fileList[0] = model;
+
+
+            var testList = new DocumentModel[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                var testFile = new DocumentModel();
+                testFile.Filename = "2" + new Random().Next(9000000, 9999999).ToString() + ".pdf";
+                testList[i] = testFile;
+            }
 
             var creationService = new XmlCreationService("10002", fileList);
             creationService.CreateXml();
