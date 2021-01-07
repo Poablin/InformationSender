179d200ef89203e48a4bb9a306dcd97cddbe1def
Poablin <65161335+Poablin@users.noreply.github.com>
2021-01-07 12:28:58 +0100

    Update XmlCreationService.cs

--- InformationSender/XmlCreationService.cs
+++ InformationSender/XmlCreationService.cs
@@ -50,25 +50,3 @@ namespace InformationSender
         }
     }
 }
-
-
-
-//var xmlTree1 = new XElement("File",
-//new XElement("Owner", 1),
-//new XElement("DocType", 2)
-//);
-
-//var xmlTree2 =
-//new XElement("FileInfo",
-//    new XElement("Filename", 3),
-//    new XElement("InvoiceNumber", 4),
-//    new XElement("CustomerNumber", 5),
-//    new XElement("Name", 6),
-//    new XElement("Addr1", 7),
-//    new XElement("ZipCode", 8),
-//    new XElement("ZipName", 9),
-//    new XElement("CountryCode", 10),
-//    new XElement("IssueDate", 11),
-//    new XElement("DueDate", 12),
-//    new XElement("TotalAmount", 13)
-//);
