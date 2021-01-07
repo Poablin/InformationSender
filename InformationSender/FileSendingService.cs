ef5c2fab8d3127e97498f3652c604bc55ba726fb
Poablin <65161335+Poablin@users.noreply.github.com>
2021-01-07 19:11:51 +0100

    Update FileSendingService.cs

--- InformationSender/FileSendingService.cs
+++ InformationSender/FileSendingService.cs
@@ -1,27 +1,31 @@
 ﻿using Renci.SshNet;
-public static class SendFileToServer
-{
-    // Enter your host name or IP here
-    private static string host = "127.0.0.1";
-    // Enter your sftp username here
-    private static string username = "sftp";
-    // Enter your sftp password here
-    private static string password = "12345";
-    public static int Send(string fileName)
-    {
-        var connectionInfo = new ConnectionInfo(host, 22, "sftp", new PasswordAuthenticationMethod(username, password));
-        // Upload File
-        using (var sftp = new SftpClient(connectionInfo))
-        {
 
-            sftp.Connect();
-            //sftp.ChangeDirectory("/MyFolder");
-            using (var uplfileStream = System.IO.File.OpenRead(fileName))
+namespace InformationSender
+{
+    public static class SendFileToServer
+    {
+        // Enter your host name or IP here
+        private static string host = "127.0.0.1";
+        // Enter your sftp username here
+        private static string username = "sftp";
+        // Enter your sftp password here
+        private static string password = "12345";
+        public static int Send(string fileName)
+        {
+            var connectionInfo = new ConnectionInfo(host, 22, "sftp", new PasswordAuthenticationMethod(username, password));
+            // Upload File
+            using (var sftp = new SftpClient(connectionInfo))
             {
-                sftp.UploadFile(uplfileStream, fileName, true);
+
+                sftp.Connect();
+                //sftp.ChangeDirectory("/MyFolder");
+                using (var uplfileStream = System.IO.File.OpenRead(fileName))
+                {
+                    sftp.UploadFile(uplfileStream, fileName, true);
+                }
+                sftp.Disconnect();
             }
-            sftp.Disconnect();
+            return 0;
         }
-        return 0;
     }
 }
