675aaaeccac144b905a81d039365a9aeecc2b122
Poablin <65161335+Poablin@users.noreply.github.com>
2021-01-07 17:45:07 +0100

    Update FileCreationService.cs

--- InformationSender/FileCreationService.cs
+++ InformationSender/FileCreationService.cs
@@ -19,41 +19,53 @@ namespace InformationSender
 
         public void CreateFilesFromFileInfo()
         {
-            var ownerList = FileList.Select(x => x.Owner).Distinct();
-
-            foreach (var owner in ownerList)
+            try
             {
-                var doc = new XDocument(new XElement("File"));
-                doc.Root.Add(new XElement("Owner", owner));
-                doc.Root.Add(new XElement("DocType", "BOF"));
+                var ownerList = FileList.Select(x => x.Owner).Distinct();
 
-                var filesToAddIntoXml = FileList.Where(x => x.Owner == owner);
-
-                foreach (var file in filesToAddIntoXml)
+                foreach (var owner in ownerList)
                 {
-                    var fileElement =
-                    new XElement("FileInfo",
-                       new XElement("Filename", file.Filename),
-                       new XElement("InvoiceNumber", file.InvoiceNumber),
-                       new XElement("CustomerNumber", file.CustomerNumber),
-                       new XElement("Name", file.Name),
-                       new XElement("Addr1", file.Addr1),
-                       new XElement("ZipCode", file.ZipCode),
-                       new XElement("ZipName", file.ZipName),
-                       new XElement("CountryCode", file.CountryCode),
-                       new XElement("IssueDate", file.IssueDate),
-                       new XElement("DueDate", file.DueDate),
-                       new XElement("TotalAmount", file.TotalAmount));
-                    doc.Root.Add(fileElement);
-                }
-                var sw = new StringWriter();
-                using (XmlWriter xw = XmlWriter.Create(@"E:\Test\" + Random.Next(999999) + ".xml"))
-                {
-                    doc.Save(xw);
-                }
-                sw.Close();
+                    Directory.CreateDirectory(@"E:\Test\" + owner);
 
-                ZipFile.CreateFromDirectory(@"E:\Test\" + owner, @"E:\Test\" + owner + ".zip");
+                    var doc = new XDocument(new XElement("File"));
+                    doc.Root.Add(new XElement("Owner", owner));
+                    doc.Root.Add(new XElement("DocType", "BOF"));
+
+                    var filesToAddIntoXml = FileList.Where(x => x.Owner == owner);
+
+                    foreach (var file in filesToAddIntoXml)
+                    {
+                        var fileElement =
+                        new XElement("FileInfo",
+                           new XElement("Filename", file.Filename),
+                           new XElement("InvoiceNumber", file.InvoiceNumber),
+                           new XElement("CustomerNumber", file.CustomerNumber),
+                           new XElement("Name", file.Name),
+                           new XElement("Addr1", file.Addr1),
+                           new XElement("ZipCode", file.ZipCode),
+                           new XElement("ZipName", file.ZipName),
+                           new XElement("CountryCode", file.CountryCode),
+                           new XElement("IssueDate", file.IssueDate),
+                           new XElement("DueDate", file.DueDate),
+                           new XElement("TotalAmount", file.TotalAmount));
+                        doc.Root.Add(fileElement);
+                        File.WriteAllText(@"E:\Test\" + owner + "\\" + file.Filename, "");
+                    }
+
+                    var sw = new StringWriter();
+                    using (XmlWriter xw = XmlWriter.Create(@"E:\Test\" + owner + "\\" + Random.Next(999999) + ".xml"))
+                    {
+                        doc.Save(xw);
+                    }
+                    sw.Close();
+
+                    ZipFile.CreateFromDirectory(@"E:\Test\" + owner, @"E:\Test\" + owner + ".zip");
+                    Directory.Delete(@"E:\Test\" + owner, true);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
     }
