4be6a0e6a9b3cc343dc6cd967939f0b426514029
Poablin <65161335+Poablin@users.noreply.github.com>
2021-01-07 20:23:11 +0100

    Trenger sikker format sjekker

---
+++ InformationSender/FileInfoFormatValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InformationSender
+{
+    static class FileInfoFormatValidator
+    {
+        public static bool IsValidFormat(FileModel fileinfo)
+        {
+            return true;
+        }
+    }
+}
--- InformationSender/FileInfoGenerator.cs
+++ InformationSender/FileInfoGenerator.cs
@@ -14,24 +14,26 @@ namespace InformationSender
             {
                 var date = RandomDate();
                 var fileInfo = new FileModel();
-                fileInfo.Owner = Random.Next(10001, 10004).ToString();
+                fileInfo.Owner = Random.Next(10001, 10003).ToString();
                 fileInfo.DocType = "BOF";
-                fileInfo.Filename = "2" + Random.Next(1000000, 9999999) + ".pdf";
-                fileInfo.InvoiceNumber = "1" + Random.Next(10000, 99999);
+                fileInfo.Filename = "2" + Random.Next(0000000, 9999999) + ".pdf";
+                fileInfo.InvoiceNumber = "1" + Random.Next(00000, 99999);
                 fileInfo.CustomerNumber = "000001";
                 //Husk Mod10 på KID
                 fileInfo.KID = $"005{fileInfo.InvoiceNumber}6";
                 fileInfo.Name = RandomString(10);
                 fileInfo.Addr1 = RandomString(6);
-                fileInfo.ZipCode = Random.Next(1000, 9999).ToString();
+                fileInfo.ZipCode = Random.Next(0000, 9999).ToString();
                 fileInfo.ZipName = RandomString(9);
                 fileInfo.CountryCode = "NO";
                 fileInfo.IssueDate = $"{date.Day}.{date.Month}.{date.Year}";
                 date = date.AddDays(30);
                 fileInfo.DueDate = $"{date.Day}.{date.Month}.{date.Year}";
-                fileInfo.TotalAmount = Random.Next(10000, 99999);
+                fileInfo.TotalAmount = Random.Next(00000, 99999);
                 fileInfo.FileLocation = @"E:\Test\";
                 fileInfo.BatchID = 001;
+
+                //if (!FileInfoFormatValidator.IsValidFormat(fileInfo)) continue;
 
                 fileInfoList[i] = fileInfo;
             }
