3800919721daf3127595c8c9ed89410c38f5bbf8
Poablin <65161335+Poablin@users.noreply.github.com>
2021-01-07 11:30:17 +0100

    Initial commit

---
+++ InformationSender/DocumentModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InformationSender
+{
+    public class DocumentModel
+    {
+        // Either: 10001 or 10002
+        public string Owner { get; set; }
+
+        // Always "BOF"
+        public string DocType { get; set; }
+
+        // Unique number + .pdf in format 2XXXXXXX.pdf
+        public string Filename { get; set; }
+
+        // Unique number in format 1XXXXXX 
+        public string InvoiceNumber { get; set; }
+
+        // Always 000001
+        public string CustomerNumber { get; set; }
+
+        // A valid KID using MOD 10 check digit, in format 005{invoiceNumber}{checkDigit}
+        public string KID { get; set; }
+
+        // A random sequence of alpha characters
+        public string Name { get; set; }
+
+        // A random sequence of alpha characters
+        public string Addr1 { get; set; }
+
+        // A random 4 digit number
+        public string ZipCode { get; set; }
+
+        // A random sequence of alpha characters
+        public string ZipName { get; set; }
+
+        // Always NO
+        public string CountryCode { get; set; }
+
+        // A random date, minimum 30 days back
+        public string IssueDate { get; set; }
+
+        // Always IssueDate + 30 days
+        public string DueDate { get; set; }
+
+        // A random number
+        public double TotalAmount { get; set; }
+
+        // Some local path
+        public string FileLocation { get; set; }
+
+        // Always 001
+        public long BatchID { get; set; }
+
+    }
+}
---
+++ InformationSender/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace InformationSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentModel model = new DocumentModel();
+
+            XElement xmlTree1 = new XElement("File",
+            new XElement("Owner", 1),
+            new XElement("DocType", 2),
+            new XElement("FileInfo",
+                new XElement("Filename", 3),
+                new XElement("InvoiceNumber", 4),
+                new XElement("CustomerNumber", 5),
+                new XElement("Name", 6),
+                new XElement("Addr1", 7),
+                new XElement("ZipCode", 8),
+                new XElement("ZipName", 9),
+                new XElement("CountryCode", 10),
+                new XElement("IssueDate", 11),
+                new XElement("DueDate", 12),
+                new XElement("TotalAmount", 13)
+                )
+            );
+
+            XDocument doc = new XDocument(xmlTree1);
+
+            //XmlWriterSettings settings = new XmlWriterSettings();
+            //settings.OmitXmlDeclaration = true;
+            StringWriter sw = new StringWriter();
+            using (XmlWriter xw = XmlWriter.Create(@"C:\Users\krist\Downloads\Test.xml"))
+            {
+                doc.Save(xw);
+            }
+        }
+    }
+}
