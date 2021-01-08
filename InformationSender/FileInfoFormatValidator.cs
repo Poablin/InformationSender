7c7d69d544c77224aaf702548e157afcfec8b2a6
Poablin <65161335+Poablin@users.noreply.github.com>
2021-01-08 11:33:54 +0100

    Update FileInfoFormatValidator.cs

--- InformationSender/FileInfoFormatValidator.cs
+++ InformationSender/FileInfoFormatValidator.cs
@@ -11,16 +11,14 @@ namespace InformationSender
             new Regex(@"^2[\d]{7}.pdf$", RegexOptions.Compiled);
 
         private static readonly Regex ValidInvoiceNumberRegex =
-        new Regex(@"^1[\d]{6}$", RegexOptions.Compiled);
-
-
+            new Regex(@"^1[\d]{6}$", RegexOptions.Compiled);
 
         public static bool IsValidFormat(FileModel fileinfo)
         {
             var filenameCorrectFormat = ValidFilenameRegex.Match(fileinfo.Filename);
             var invoiceNumberCorrectFormat = ValidInvoiceNumberRegex.Match(fileinfo.InvoiceNumber);
 
-            var match = filenameCorrectFormat.Success 
+            var match = filenameCorrectFormat.Success
                 && invoiceNumberCorrectFormat.Success;
 
             return match;
